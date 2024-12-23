72f7b05081af10754fbbe0e8db7e6d23f653cf96
user3301 <zhelun.gai@anz.com>
2024-12-24 10:50:13 +1100

    update wezterm config

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -9,8 +9,10 @@ local config = wezterm.config_builder()
 -- For example, changing the color scheme:
 config.color_scheme = "Gruvbox Dark (Gogh)"
 
-config.font = wezterm.font("ZedMono Nerd Font", { bold = false, italic = false })
-config.font_size = 16
+config.window_decorations = "INTEGRATED_BUTTONS"
+
+config.font = wezterm.font("JetBrains Mono", { bold = false, italic = false })
+config.font_size = 14.5
 
 config.initial_rows = 40
 config.initial_cols = 120
