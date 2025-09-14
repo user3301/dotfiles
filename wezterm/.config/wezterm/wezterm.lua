bc5d1afd73c79a43b7c17b416bf0ab247e8370ae
user3301 <zhelun.gai@anz.com>
2025-09-14 18:54:53 +1000

    add leader key for wezterm and config pane split shortcuts

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -19,5 +19,57 @@ config.font_size = 14.5
 config.initial_rows = 40
 config.initial_cols = 120
 
+config.leader = {
+	key = "a",
+	mods = "CTRL",
+	timeout_milliseconds = 1000,
+}
+
+-- ⌨️ Key bindings that use the leader key
+config.keys = {
+	-- Split pane horizontally: leader + "
+	{
+		key = '"',
+		mods = "LEADER",
+		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+
+	-- Split pane vertically: leader + %
+	{
+		key = "%",
+		mods = "LEADER",
+		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+
+	-- Close current pane with confirmation: leader + x
+	{
+		key = "x",
+		mods = "LEADER",
+		action = wezterm.action.CloseCurrentPane({ confirm = true }),
+	},
+
+	-- Move between panes using vim-style keys
+	{
+		key = "h",
+		mods = "LEADER",
+		action = wezterm.action.ActivatePaneDirection("Left"),
+	},
+	{
+		key = "l",
+		mods = "LEADER",
+		action = wezterm.action.ActivatePaneDirection("Right"),
+	},
+	{
+		key = "k",
+		mods = "LEADER",
+		action = wezterm.action.ActivatePaneDirection("Up"),
+	},
+	{
+		key = "j",
+		mods = "LEADER",
+		action = wezterm.action.ActivatePaneDirection("Down"),
+	},
+}
+
 -- and finally, return the configuration to wezterm
 return config
