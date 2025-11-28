3c03ac4f3e19bbf3d200ee4132f269636e30f9c6
user3301 <zhelun.gai@anz.com>
2025-11-28 23:27:47 +1100

    make nvim background transparent to inherit terminal settings

--- nvim/.config/nvim/lua/plugins/colorschemes.lua
+++ nvim/.config/nvim/lua/plugins/colorschemes.lua
@@ -9,4 +9,18 @@ return {
       colorscheme = "minischeme",
     },
   },
+  -- Make background transparent to inherit terminal settings
+  {
+    "LazyVim/LazyVim",
+    opts = function()
+      vim.api.nvim_create_autocmd("ColorScheme", {
+        pattern = "*",
+        callback = function()
+          vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+          vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+          vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+        end,
+      })
+    end,
+  },
 }
