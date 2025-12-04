6cbd85b1cec3577c98a7988ef5027d87c7fd29ca
user3301 <stan_gai@hotmail.com>
2025-12-05 10:40:29 +1100

    config git

--- home/git.nix
+++ home/git.nix
@@ -5,33 +5,34 @@
   # Currently using minimal config - git is installed via packages.nix
 
   # Uncomment and customize to manage git config via home-manager:
-  # programs.git = {
-  #   enable = true;
-  #   userName = "Your Name";
-  #   userEmail = "your.email@example.com";
-  #
-  #   extraConfig = {
-  #     init.defaultBranch = "main";
-  #     core.editor = "nvim";
-  #     pull.rebase = false;
-  #
-  #     # Useful aliases
-  #     alias = {
-  #       st = "status";
-  #       co = "checkout";
-  #       br = "branch";
-  #       ci = "commit";
-  #       lg = "log --graph --oneline --all";
-  #     };
-  #   };
-  #
-  #   # Git ignore patterns
-  #   ignores = [
-  #     ".DS_Store"
-  #     "*.swp"
-  #     "*.swo"
-  #     "*~"
-  #     ".direnv/"
-  #   ];
-  # };
+  programs.git = {
+    enable = true;
+    userName = "user3301";
+    userEmail = "stan_gai@hotmail.com";
+
+    extraConfig = {
+      init.defaultBranch = "main";
+      core.editor = "nvim";
+      pull.rebase = false;
+
+      # Useful aliases
+      # inherit aliases from omz
+      # alias = {
+      #   st = "status";
+      #   co = "checkout";
+      #   br = "branch";
+      #   ci = "commit";
+      #   lg = "log --graph --oneline --all";
+      # };
+    };
+
+    # Git ignore patterns
+    ignores = [
+      ".DS_Store"
+      "*.swp"
+      "*.swo"
+      "*~"
+      ".direnv/"
+    ];
+  };
 }
