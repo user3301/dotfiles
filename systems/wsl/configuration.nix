829bb244dad08ba4d1ad19b047667d97b19d70af
user3301 <stan_gai@hotmail.com>
2025-12-15 21:57:28 +1100

    enable nix-ld
    asdf installs prebuilt Node.js binaries that are:
    
    dynamically linked
    
    built for generic Linux (FHS)
    
    On NixOS, those binaries cannot find the dynamic linker / glibc

--- systems/wsl/configuration.nix
+++ systems/wsl/configuration.nix
@@ -42,6 +42,9 @@
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
+  # to keep asdf dynamically linked executables runnable
+  programs.nix-ld.enable = true;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It's perfectly fine and recommended to leave
