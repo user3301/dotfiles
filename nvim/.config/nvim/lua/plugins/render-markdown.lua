47a7d4b1bb6ee941c9ae00fcc7ac73ae502bb318
user3301 <26126682+user3301@users.noreply.github.com>
2026-01-09 23:07:46 +1100

    markdown nvim update

--- nvim/.config/nvim/lua/plugins/render-markdown.lua
+++ nvim/.config/nvim/lua/plugins/render-markdown.lua
@@ -2,7 +2,47 @@ return {
   {
     "MeanderingProgrammer/render-markdown.nvim",
     dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-mini/mini.nvim" },
-    opts = {},
+    opts = {
+      -- Customize heading icons for different levels
+      heading = {
+        -- Turn on / off heading icon & background rendering
+        enabled = true,
+        -- Turn on / off any sign column related rendering
+        sign = true,
+        -- Replaces '#+' of 'atx_h._marker'
+        -- The number of '#' in the heading determines the 'level'
+        -- The 'level' is used to index into the array using a cycle
+        -- Filled arrow icons for visual hierarchy
+        icons = { "󰼏 ", "󰼐 ", "󰼑 ", "󰼒 ", "󰼓 ", "󰼔 " },
+        -- Added to the sign column if enabled
+        -- The 'level' is used to index into the array using a cycle
+        signs = { "󰫎 " },
+        -- Width of the heading background:
+        --  block: width of the heading text
+        --  full:  full width of the window
+        width = "full",
+        -- The 'level' is used to index into the array using a clamp
+        -- Highlight for the heading icon and extends through the entire line
+        backgrounds = {
+          "RenderMarkdownH1Bg",
+          "RenderMarkdownH2Bg",
+          "RenderMarkdownH3Bg",
+          "RenderMarkdownH4Bg",
+          "RenderMarkdownH5Bg",
+          "RenderMarkdownH6Bg",
+        },
+        -- The 'level' is used to index into the array using a clamp
+        -- Highlight for the heading and sign icons
+        foregrounds = {
+          "RenderMarkdownH1",
+          "RenderMarkdownH2",
+          "RenderMarkdownH3",
+          "RenderMarkdownH4",
+          "RenderMarkdownH5",
+          "RenderMarkdownH6",
+        },
+      },
+    },
     cmd = { "RenderMarkdown" },
     ft = { "markdown", "vimwiki" },
   },
