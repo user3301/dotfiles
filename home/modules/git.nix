99f4318992d8dfe15648fd04a4056ced9d682812
user3301 <26126682+user3301@users.noreply.github.com>
2026-06-14 22:35:56 +1000

    add delta for git diff

--- home/modules/git.nix
+++ home/modules/git.nix
@@ -6,6 +6,7 @@
     git
     gh # GitHub CLI
     lazygit
+    delta # syntax-highlighting pager for git diffs
   ];
 
   # GPG
@@ -21,6 +22,11 @@
   # Symlinks entire git config directory to include config.local and other files
   xdg.configFile."git".source = config.lib.file.mkOutOfStoreSymlink
     "${config.home.homeDirectory}/dotfiles/git/.config/git";
+
+  # Lazygit configuration via XDG config symlink (delta paging, etc.)
+  # Symlinked so it can also be managed with GNU Stow on non-NixOS distros
+  xdg.configFile."lazygit".source = config.lib.file.mkOutOfStoreSymlink
+    "${config.home.homeDirectory}/dotfiles/lazygit/.config/lazygit";
 
   # GitHub CLI
   programs.gh = {
